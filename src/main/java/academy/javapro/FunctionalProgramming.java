cdfe59b2a63a72bb17694027da1ad54f02f5fecf
UmairMahmood404 <117422319+UmairMahmood404@users.noreply.github.com>
2025-02-03 07:32:27 -0500

    finished homework

--- src/main/java/academy/javapro/FunctionalProgramming.java
+++ src/main/java/academy/javapro/FunctionalProgramming.java
@@ -23,12 +23,15 @@ public class FunctionalProgramming {
         // Hint: Use .map(n -> ...)
         List<Integer> doubledNumbers = numbers.stream()
             // Write your code here
+                .map(n -> n * 2)
             .collect(Collectors.toList());
 
         // TASK 3: Sum all numbers in doubledNumbers
         // TODO: Create a stream that sums all numbers
         // Hint: Use .mapToInt(n -> n).sum()
-        int sum = 0; // Replace with your stream code
+        int sum = doubledNumbers.stream()
+                .mapToInt(n -> n)  // Convert to IntStream
+                .sum();; // Replace with your stream code
 
         // Print results
         System.out.println("Doubled numbers: " + doubledNumbers);
--- src/main/java/academy/javapro/StudentStreamLab.java
+++ src/main/java/academy/javapro/StudentStreamLab.java
@@ -1,5 +1,6 @@
 package academy.javapro;
-
+//Umair Mahmood
+//CUS1168
 import java.util.*;
 import java.util.stream.*;
 
@@ -15,11 +16,33 @@ public class StudentStreamLab {
         );
 
         // TODO - Filtering: Students with GPA > 3.0
+        List<Student> highGpaStudents = students.stream()
+                .filter(s -> s.getGpa() > 3.0)
+                .collect(Collectors.toList());
+        System.out.println("Students with GPA > 3.0:");
+        highGpaStudents.forEach(System.out::println);
+        System.out.println();
 
         // TODO - Mapping: Extract names of students with GPA > 3.0
+        List<String> highGpaStudentnames = highGpaStudents.stream()
+                .map(Student::getName)
+                .collect(Collectors.toList());
 
+        System.out.println("High GPA student names:");
+        highGpaStudentnames.forEach(System.out::println);
+        System.out.println();
         // TODO - Reducing: Calculate the average GPA of all students
-
+        double averageGpa = students.stream()
+                .mapToDouble(Student::getGpa)
+                .average()
+                .orElse(0.0);
+        System.out.printf("Average GPA: %.2f\n\n", averageGpa);
         // TODO Collecting: Collect all "Junior" students into a list
+        List<Student> juniors = students.stream()
+                .filter(s -> s.getCollegeYear().equals("Junior"))
+                .collect(Collectors.toList());
+
+        System.out.println("Juniors:");
+        juniors.forEach(System.out::println);
     }
 }
